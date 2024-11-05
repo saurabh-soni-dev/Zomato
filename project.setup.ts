1426f289de1507ee18c65e1e955a9ec65c923924
saurabh-soni-dev <saurabh.soni6408@gmail.com>
2024-11-05 23:30:41 +0530

    Add app icon

---
+++ project.setup.ts
@@ -0,0 +1,24 @@
+// ==============================
+// Completed Setup
+// ==============================
+// 1. Create project using:
+//    npx @react-native-community/cli@latest init Zomato
+// 2. Add App Icon:
+//    - Android: Replace with created icons folder
+//    - iOS: Replace icons image in Xcode
+// 3. Add fonts for both iOS & Android:
+//    - Link using this command: npm i react-native-asset
+// 4. Typescript config files:
+//    - tsconfig.json, metro.config.js, babel.config.js
+
+// ==============================
+// Not Completed Setup
+// ==============================
+// 5. Add Splash screen for both:
+//    - Android:
+//    - iOS:
+// 6. Add required scripts in the package.json file
+// 7. Add icon plugins like Ionicons, MaterialCommunityIcons, and Material Icons
+// 8. Add Global files:
+// 9. Add all required folders and files (log, navigationRef, validation, keyboardListeners, api)
+// 10.
